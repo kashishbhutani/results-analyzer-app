87a95d972959e90b9e2eb623ea17fe04d137a9c3
Kashish Bhutani <kashishbhutaniofficial@gmail.com>
2023-03-21 23:32:25 +0530

    Added Service Level Specs For Daily Results Data

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,17 @@
 FactoryBot.define do
-  factory :results_data do
-    subject { Faker::Name.name }
+  factory :results_data_one, class: 'ResultsData' do
+    subject { 'Science' }
     marks { 72.25 }
-    timestamp { '2022-04-18 12:01:35.678' }
+    timestamp { DateTime.now.to_s }
+  end
+  factory :results_data_two, class: 'ResultsData' do
+    subject { 'Science' }
+    marks { 89.25 }
+    timestamp { DateTime.now.to_s }
+  end
+  factory :results_data_three, class: 'ResultsData' do
+    subject { 'Science' }
+    marks { 69.25 }
+    timestamp { DateTime.now.to_s }
   end
 end
--- spec/services/results_aggregation_service_spec.rb
+++ spec/services/results_aggregation_service_spec.rb
@@ -2,6 +2,28 @@
 
 RSpec.describe ResultsAggregationService do
   context 'daily_result_stats' do
+    # Preparing the data
+    let!(:results_data_one) { FactoryBot.create(:results_data_one) }
+    let!(:results_data_two) { FactoryBot.create(:results_data_two) }
+    let!(:results_data_three) { FactoryBot.create(:results_data_three) }
+
+    it "should calculate the daily results stats if there's results data present" do
+      result = ResultsAggregationService.daily_result_stats
+
+      expect(result).not_to be_empty
+      expect(result['Science'].count).to eq(3)
+      expect(DailyResultStat.count).to eq(1)
+    end
+
+    it "should not calculate the daily results stats if there's no results data present" do
+      # Deleting all the results_data records
+      ResultsData.delete_all
+
+      result = ResultsAggregationService.daily_result_stats
+
+      expect(result).to be_nil
+      expect(DailyResultStat.count).to eq(0)
+    end
   end
 
   context 'monthly_average_result_stats' do
