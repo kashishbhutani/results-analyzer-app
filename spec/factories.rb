28500ca574d0fd64efa5cf5b956410aa2d441892
Kashish Bhutani <kashishbhutaniofficial@gmail.com>
2023-03-22 00:15:10 +0530

    Added Specs For Monthly Average Service | Code Improvements

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,16 +2,37 @@ FactoryBot.define do
   factory :results_data_one, class: 'ResultsData' do
     subject { 'Science' }
     marks { 72.25 }
-    timestamp { DateTime.now.to_s }
+    timestamp { Date.today.to_s }
   end
   factory :results_data_two, class: 'ResultsData' do
     subject { 'Science' }
     marks { 89.25 }
-    timestamp { DateTime.now.to_s }
+    timestamp { Date.today.to_s }
   end
   factory :results_data_three, class: 'ResultsData' do
     subject { 'Science' }
     marks { 69.25 }
-    timestamp { DateTime.now.to_s }
+    timestamp { Date.today.to_s }
+  end
+  factory :daily_result_stats_one, class: 'DailyResultStat' do
+    date { Date.today.to_s }
+    subject { 'Science' }
+    daily_low { 119.88 }
+    daily_high { 126.76 }
+    result_count { 81 }
+  end
+  factory :daily_result_stats_two, class: 'DailyResultStat' do
+    date { Date.yesterday.to_s }
+    subject { 'Science' }
+    daily_low { 123.73 }
+    daily_high { 127.23 }
+    result_count { 57 }
+  end
+  factory :daily_result_stats_three, class: 'DailyResultStat' do
+    date { (Date.yesterday - 1).to_s }
+    subject { 'Science' }
+    daily_low { 120.27 }
+    daily_high { 123.33 }
+    result_count { 75 }
   end
 end
--- spec/services/results_aggregation_service_spec.rb
+++ spec/services/results_aggregation_service_spec.rb
@@ -12,7 +12,11 @@ RSpec.describe ResultsAggregationService do
 
       expect(result).not_to be_empty
       expect(result['Science'].count).to eq(3)
+
+      daily_result_stat = DailyResultStat.last
+
       expect(DailyResultStat.count).to eq(1)
+      expect(daily_result_stat.result_count).to eq(3)
     end
 
     it "should not calculate the daily results stats if there's no results data present" do
@@ -27,5 +31,33 @@ RSpec.describe ResultsAggregationService do
   end
 
   context 'monthly_average_result_stats' do
+    # Preparing the data
+    let!(:daily_result_stats_one) { FactoryBot.create(:daily_result_stats_one) }
+    let!(:daily_result_stats_two) { FactoryBot.create(:daily_result_stats_two) }
+    let!(:daily_result_stats_three) { FactoryBot.create(:daily_result_stats_three) }
+
+    it "should calculate the monthly average data if there's daily results stats data present" do
+      result = ResultsAggregationService.monthly_average_result_stats
+
+      expect(result).not_to be_empty
+      expect(result['Science'].count).to eq(3)
+
+      monthly_avg = MonthlyAverage.last
+
+      expect(MonthlyAverage.count).to eq(1)
+      expect(monthly_avg.monthly_result_count_used).to eq(
+        daily_result_stats_one.result_count + daily_result_stats_two.result_count + daily_result_stats_three.result_count
+      )
+    end
+
+    it "should not calculate the monthly average data if there's no daily results stats data present" do
+      # Deleting all the results_data records
+      DailyResultStat.delete_all
+
+      result = ResultsAggregationService.monthly_average_result_stats
+
+      expect(result).to be_nil
+      expect(MonthlyAverage.count).to eq(0)
+    end
   end
 end
